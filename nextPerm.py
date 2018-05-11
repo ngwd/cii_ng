e40dc1397678a7b365e67bac887ae73061bdbed6
ngwd <ngwuedag@gmail.com>
2018-05-11 00:13:40 -0700

    Update nextPerm.py

--- nextPerm.py
+++ nextPerm.py
@@ -1,9 +1,17 @@
 class nextPerm:
+    """
+    can deal with first list as 
+    [1, 2, 3]
+    [2, 2, 8]
+    ['a1', 'b', 'a1', 'z']
+    """
     def __init__(self, perm0):
-        self.kthPerm = perm0 
-        self.weights = {v:i for i, v in enumerate(perm0)}
-    def nextPermutation(self):
+        self.kthPerm = perm0
+        self.weights = {v:i for i, v in enumerate(perm0)} 
+    def nextPermutation(self, reverse=False):
         """
+        reverse: False  means ascending, i.e. next
+        reverse: True   means descending,i.e. last
         beginning is 1234
         kth   (k+1)th
         4321  1234        #0, scan reversely, find no leftward downslide
@@ -25,11 +33,14 @@ class nextPerm:
                               than ramp is 4, swap 3, 4 get 1432, then reverse the array
                               of [peak's previous index, end), get 1423
         """
+        def cmp0(a,b,reverse):
+            return a<b if reverse else a>b
+
         nums, w = self.kthPerm, self.weights
         
         i = len(nums) - 1
         while i>0:
-            if w[nums[i]]>w[nums[i-1]]:
+            if cmp0(w[nums[i]],w[nums[i-1]], reverse):
                 break
             i -= 1
         if i==0: # no leftward downslide
@@ -40,30 +51,7 @@ class nextPerm:
         # in nums(ramp, end), find the last values > weights[nums[ramp]]
         i = len(nums) - 1
         while i>ramp:
-            if w[nums[i]]> w[nums[ramp]]:
-                nums[i], nums[ramp] =  nums[ramp], nums[i] 
-                nums = nums[:peak] + nums[peak:][::-1]
-                break
-            i -=1
-        self.kthPerm = nums
-        return self.kthPerm
-
-    def lastPermutation(self):
-        nums, w = self.kthPerm, self.weights
-        i = len(nums) - 1
-        while i>0:
-            if w[nums[i]] < w[nums[i-1]]:
-                break
-            i -= 1
-        if i==0: # no rightward downslide
-            self.kthPerm = nums[::-1]
-            return self.kthPerm
-
-        ramp, peak = i-1, i
-        # in nums(ramp, end), find the last values > weights[nums[ramp]]
-        i = len(nums) - 1
-        while i>ramp:
-            if w[nums[i]] < w[nums[ramp]]:
+            if cmp0(w[nums[i]],w[nums[ramp]], reverse):
                 nums[i], nums[ramp] =  nums[ramp], nums[i] 
                 nums = nums[:peak] + nums[peak:][::-1]
                 break
@@ -75,10 +63,5 @@ b = [1,2,3,4] # even b = ['a', 'bz', 'xyz'] can be permutated properly
 a = nextPerm(b)
 i = 1
 while i<24:
-    print(a.nextPermutation())
-    i += 1
-i = 1
-print("reverse ")
-while i<24:
-    print(a.lastPermutation())
+    print(a.nextPermutation(True))
     i += 1
